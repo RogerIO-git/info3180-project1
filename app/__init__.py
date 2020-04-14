0a89abd9a8412fb8e8c6ed98970d6848d6ea9459
Rogon101 <rogonjones@gmail.com>
2020-04-14 16:04:05 -0500

    Heroku Updated

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost/info3180_project_1"
-app.config['SQLALCHEMY_DATABASE_URI']='postgres:://odmeryjflnxmph:a78e3ed20280cc577698344a03a36c62a362c3bcc7a5c14d25b54ddd6d3fe76f@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d41e4kl2ts2u0i'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://odmeryjflnxmph:a78e3ed20280cc577698344a03a36c62a362c3bcc7a5c14d25b54ddd6d3fe76f@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d41e4kl2ts2u0i'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 
